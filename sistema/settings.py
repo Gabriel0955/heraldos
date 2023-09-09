ee7542b2e7fc0751920e00f3d4d152b49b2f44ed
Gabriel Aguirre <108961289+Gabriel0955@users.noreply.github.com>
2023-09-09 18:55:42 -0500

    Update settings.py

--- sistema/settings.py
+++ sistema/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'cwc9m+z#ppu8)+@v#$7ci$=w*gae$+%n&gfq6kcsn#59ze(3a3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['heraldos.onrender.com']
 
 
 # Application definition
